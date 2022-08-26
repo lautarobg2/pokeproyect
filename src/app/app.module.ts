1f93573317a0bdf59b6829ce4372c932eb892feb
lautarobg2 <lautiibogado@gmail.com>
2022-08-25 22:10:57 -0300

    navbar, colores y algo mas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TiposComponent } from './components/tipos/tipos.component';
+import { SingleComponent } from './components/single/single.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { CardComponent } from './components/home/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TiposComponent,
+    SingleComponent,
+    AboutComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
